e0fbdc7e4035bfeb90da5cfc6bd69b8f6a2b361f
blliav <bll.eliav@gmail.com>
2025-10-28 20:26:13 +0200

    refactor: remove unused extension.json configuration file
    - Deleted PyArea.extention/extension.json file that contained unused metadata
    - Removed configuration that was no longer needed for the extension system
    - Note: Fixed typo in directory name ("extention" vs "extension")

---
+++ pyArea.tab/Utilities.panel/AreaTools.stack/FixDwfx.pushbutton/FixDwfx_script.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Remove white background from DWFX floor plans."""
+
+__title__ = "Fix\nDWFX"
+__author__ = "Your Name"
+
+from pyrevit import revit, DB, forms
+
+# Placeholder for Fix DWFX functionality
+def fix_dwfx_background():
+    """
+    Remove white background from DWFX exports in floor plans.
+    
+    This tool will:
+    1. Find all floor plan views
+    2. Remove white background settings from DWFX export
+    """
+    # TODO: Implement DWFX background removal logic
+    forms.alert("Fix DWFX - To be implemented", exitscript=True)
+
+
+if __name__ == '__main__':
+    fix_dwfx_background()
